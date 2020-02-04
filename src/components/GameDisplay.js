7bcbb15f56b8def7761038a8bcd8773c0eb84156
Michael Li <mcli.893@gmail.com>
2020-02-04 14:00:47 -0800

    add stage component to hold player's current guess

--- src/components/GameDisplay.js
+++ src/components/GameDisplay.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Glyph from './Glyph'
+
 const GameDisplay = (props) => {
+
+  const guess = [2,6,4,3]
+
   return (
     <div className='GameDisplay'>
-
+      <div className='GameDisplay-visuals'></div>
+      <div className='GameDisplay-stage'>
+        {guess.map((n,i) => (
+          <div key={i} className='stage-item'>
+            <Glyph value={n} large />
+          </div>
+        ))}
+        <div className='stage-item'>
+          <div className='button is-primary is-outlined'>
+            Submit
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
