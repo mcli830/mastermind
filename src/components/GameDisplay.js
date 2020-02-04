e50e269d12fce654c6267031dee1a690ba8f62ae
Michael Li <mcli.893@gmail.com>
2020-02-04 13:44:11 -0800

    refactor responsive sizing of gameboard

---
+++ src/components/GameDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const GameDisplay = (props) => {
+  return (
+    <div className='GameDisplay'>
+
+    </div>
+  )
+}
+
+export default GameDisplay
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,19 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
+import GameDisplay from '../components/GameDisplay'
 import GameBoard from '../components/GameBoard'
 
 const IndexPage = (props) => (
   <Layout>
     <SEO title="Home" />
     <div className="Index">
-
-      <GameBoard />
-
+      <div className='Index-gamedisplay'>
+        <GameDisplay />
+      </div>
+      <div className='Index-gameboard'>
+        <GameBoard />
+      </div>
     </div>
   </Layout>
 )
